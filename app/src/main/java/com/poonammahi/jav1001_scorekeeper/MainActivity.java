4cef9dbb559b975c5202592b077874d9a948c351
Poonam mahi <poonammahi379@gmail.com>
2021-10-29 02:27:12 -0400

    final changes done

--- app/src/main/java/com/poonammahi/jav1001_scorekeeper/MainActivity.java
+++ app/src/main/java/com/poonammahi/jav1001_scorekeeper/MainActivity.java
@@ -11,11 +11,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, RadioGroup.OnCheckedChangeListener {
 
-    private int choosedScore;
-    //private int totalScoreteam1=0;
-    //private int totalScoreteam2=0;
-    private RadioGroup increaseScoreBy;
-
+    //integer which keep track of which radiobutton is selected i.e. 1,2,4 or 6
+    //by default radiobutton 1 will be selected
+    int choosedScore=1;
+    //following variables are by default 0 and will keep track of total scores of both teams respectively
+    int totalScoreteam1=0;
+    int totalScoreteam2=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +24,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         //declare the views
+        //for the buttons
         Button team1IncreaseButton=findViewById(R.id.add_team1);
         Button team1DecreaseButton=findViewById(R.id.sub_team1);
         Button team2IncreaseButton=findViewById(R.id.add_team2);
         Button team2DecreaseButton=findViewById(R.id.sub_team2);
-
-       increaseScoreBy=findViewById(R.id.increaseScoreBy);
+        //for radiogroup
+        RadioGroup increaseScoreBy=findViewById(R.id.increaseScoreBy);
 
         //have the view listen for an event
         //onClick for the buttons
@@ -37,41 +39,53 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         team2IncreaseButton.setOnClickListener(this);
         team2DecreaseButton.setOnClickListener(this);
 
+        //onChecked for the radiogroup
         increaseScoreBy.setOnCheckedChangeListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        TextView displayScore_team1=findViewById(R.id.displayScore_team1);
-        String team1ScoreString=String.valueOf(displayScore_team1.getText());
-        int team1FinalScore=Integer.parseInt(team1ScoreString);
 
-        TextView displayScore_team2=findViewById(R.id.displayScore_team2);
-        String team2ScoreString=String.valueOf(displayScore_team2.getText());
-        int team2FinalScore=Integer.parseInt(team2ScoreString);
+        TextView displayScore_team1=findViewById(R.id.displayScore_team1); //creating object of textview(team1)
+        String team1ScoreString=String.valueOf(displayScore_team1.getText()); //getting value of textview and making sure its string
+        totalScoreteam1=Integer.parseInt(team1ScoreString); //converting value in textview to int so that we can perform add/sub operation
 
+        TextView displayScore_team2=findViewById(R.id.displayScore_team2); //creating object of textview(team2)
+        String team2ScoreString=String.valueOf(displayScore_team2.getText()); //getting value of textview and making sure its string
+        totalScoreteam2=Integer.parseInt(team2ScoreString); //converting value in textview to int so that we can perform add/sub operation
+
+        //using swtich case to perform add or sub operation on click of button
         switch(view.getId()){
-            case R.id.add_team1:
-                team1FinalScore += choosedScore;
+            case R.id.add_team1:  //onclick of add button of team1
+                totalScoreteam1 += choosedScore;  //adding select value from the radiobutton to the final score
                 break;
-            case R.id.sub_team1:
-                team1FinalScore -= choosedScore;
+            case R.id.sub_team1: //onclick of sub button of team1
+                totalScoreteam1 -= choosedScore;  //subtracting select value from the radiobutton to the final score
                 break;
-            case R.id.add_team2:
-                team2FinalScore += choosedScore;
+            case R.id.add_team2: //onclick of add button of team2
+                totalScoreteam2 += choosedScore; //adding select value from the radiobutton to the final score
                 break;
-            case R.id.sub_team2:
-                team2FinalScore -= choosedScore;
+            case R.id.sub_team2: //onclick of sub button of team1
+                totalScoreteam2 -= choosedScore; //subtracting select value from the radiobutton to the final score
                 break;
         }
-
-        displayScore_team1.setText(String.valueOf(team1FinalScore));
-        displayScore_team2.setText(String.valueOf(team2FinalScore));
+        if(totalScoreteam1<=0)  //if the score is less than 0
+        {
+            totalScoreteam1=0; //set it to 0
+        }
+        if(totalScoreteam2<=0)
+        {
+            totalScoreteam2=0;
+        }
+        displayScore_team1.setText(String.valueOf(totalScoreteam1)); //display the score(resulted value) in textview for team1
+        displayScore_team2.setText(String.valueOf(totalScoreteam2)); //display the score(resulted value) in textview for team2
     }
 
     @Override
     public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
-        int checkedButton=increaseScoreBy.getCheckedRadioButtonId();
+        int checkedButton=radioGroup.getCheckedRadioButtonId();
+
+        //switch statement get the value of which radiobutton is checked 
         switch(checkedId){
             case R.id.score1:
                 choosedScore=1;
